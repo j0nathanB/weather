695e3e5fd142f0a755b9b8fbffe1fc4306dd3e3c
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2022-12-09 00:17:39 -0800

    Add simple weather app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # weather
+## A simple weather app
+To use: 
+1. Clone repo
+2. Add API keys to index.js ([openweathermap](https://openweathermap.org/), [abstract](https://www.abstractapi.com/))
+3. Open index.html in a browser
+
+![Screenshot of app](./weather_screenshot.png?raw=true "Optional Title")
---
+++ index.js
@@ -0,0 +1,187 @@
+const OPENWEATHERMAP_API = "YOUR_API_KEY";
+const ABSTRACT_API = "YOUR_API_KEY"
+const ipUrl = `https://ipgeolocation.abstractapi.com/v1/?api_key=${ABSTRACT_API}`
+
+const weatherEmojis = {
+    '01d':'☀️',
+    '02d':'🌤️',
+    '03d':'☁️',
+    '04d':'⛅️',
+    '09d':'🌧️',
+    '10d':'🌦️',
+    '11d':'⛈️',
+    '13d':'❄️',
+    '50d':'🌫️',
+    '01n':'☀️',
+    '02n':'🌤️',
+    '03n':'☁️',
+    '04n':'⛅️',
+    '09n':'🌧️',
+    '10n':'🌦️',
+    '11n':'⛈️',
+    '13n':'❄️',
+    '50n':'🌫️',
+}
+
+const units = $('#convert-celsius').hasClass("disabled") ? 'metric' : 'imperial'
+const tempUnit = units == 'imperial' ? ' °F' : ' °C'
+const speedUnit = units == 'imperial' ? ' MPH' : ' m/s'
+
+function getFlagEmoji(countryCode) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map(char =>  127397 + char.charCodeAt());
+    return String.fromCodePoint(...codePoints);
+  }
+
+async function fetchJsonData(url) {
+    const response = await fetch(url)
+    return response.json()
+}
+
+function displayWeather(response) {
+  const tempCelsius = response.main.temp - 273.15;
+  const tempFahrenheit = tempCelsius * (9/5) + 32;
+
+  // display temp based on whether celsius or fahrenheit is selected
+  const units = $('#convert-celsius').hasClass("disabled") ? 'metric' : 'imperial'
+  const temp = units == 'metric' ? tempCelsius : tempFahrenheit
+  const speed = units == 'metric' ? response.wind.speed : response.wind.speed * 2.23694
+  const tempUnit = units == 'imperial' ? ' °F' : ' °C'
+  const speedUnit = units == 'imperial' ? ' MPH' : ' m/s'
+
+  const description = response.weather[0].main;
+  const iconCode = response.weather[0].icon;
+  const humidity = response.main.humidity;
+  const windSpeedMetric = response.wind.speed;
+  const windSpeedImperial = response.wind.speed * 2.23694;
+  
+  //Add weather description
+  $("#description").html(description);
+  $("#weather-emoji").html(weatherEmojis[iconCode]);
+
+  //Remove 'N/A' and add temperature
+  $("#current-temp").removeClass("wi wi-na");
+  $("#current-temp").html(temp.toPrecision(3) + `${tempUnit}`);
+  
+  //Add humidity and wind speed
+  $("#humidity").html(humidity + "%");
+  $("#wind-speed").html(speed.toPrecision(3) + `${speedUnit}`);
+  
+  //Temperature-dependent background images
+  if (tempFahrenheit > 70) {
+    $("body").css({
+      "background": "url(https://images.unsplash.com/uploads/1412231250505b65c703c/1fa14fb0?q=80&fm=jpg&s=66cc7c8abbf07117bd731f07b2fdc717) no-repeat center center fixed",
+      "-webkit-background-size": "cover",
+      "-moz-background-size": "cover",
+      "-o-background-size": "cover",
+      "background-size": "cover"
+    });
+  }
+  if (tempFahrenheit <= 70 && tempFahrenheit > 50) {
+    $("body").css({
+      "background": "url(https://images.unsplash.com/uploads/14126758789351371c7ec/aa322c2d?q=80&fm=jpg&s=2b648d0828ab09b915215b1003a1b633) no-repeat center center fixed",
+      "-webkit-background-size": "cover",
+      "-moz-background-size": "cover",
+      "-o-background-size": "cover",
+      "background-size": "cover"
+    });
+  }
+  if (tempFahrenheit <= 50 && tempFahrenheit > 32) {
+    $("body").css({
+      "background": "url(https://images.unsplash.com/reserve/IPEivX6xSBaiYOukY88V_DSC06462_tonemapped.jpg?q=80&fm=jpg&s=7be7c6e84b12651aa77de306dd21ff1e) no-repeat center center fixed",
+      "-webkit-background-size": "cover",
+      "-moz-background-size": "cover",
+      "-o-background-size": "cover",
+      "background-size": "cover"
+    });
+    $(".title").css("color", "rgb(50, 50, 50)")
+  }
+  if (tempFahrenheit <= 32) {
+    $("body").css({
+      "background": "url(https://images.unsplash.com/photo-1427955569621-3e494de2b1d2?q=80&fm=jpg&s=8d55df1cba9a968931f3fe05edb349c4) no-repeat center center fixed",
+      "-webkit-background-size": "cover",
+      "-moz-background-size": "cover",
+      "-o-background-size": "cover",
+      "background-size": "cover"
+    });
+    $(".title").css("color", "rgb(50, 50, 50)");
+  }
+  
+  //Celsius and Fahrenheit button group
+  $("#convert-celsius").on("click", function(e) {
+    e.preventDefault()
+    document.getElementById("current-temp").innerHTML = tempCelsius.toPrecision(3) + " °C";
+    document.getElementById("wind-speed").innerHTML = windSpeedMetric + " m/s";
+    $(this).addClass("disabled");
+    $("#convert-fahrenheit").removeClass("disabled");
+  })
+
+  $("#convert-fahrenheit").on("click", function(e) {
+    e.preventDefault()
+    document.getElementById("current-temp").innerHTML = tempFahrenheit.toPrecision(3) + " °F";
+    document.getElementById("wind-speed").innerHTML = windSpeedImperial.toPrecision(3) + " MPH";
+    $(this).addClass("disabled");
+    $("#convert-celsius").removeClass("disabled");
+  })
+}
+
+$(document).ready(function() {
+  //Uses IP address to get location data in json format
+  function displayCurrentLocation(response) {
+    const {city, region, country, country_code} = response
+    const flag = getFlagEmoji(country_code)
+    const locationDisplay = `${flag}<br>${city}, ${region},<br>${country} (${country_code})`;
+    document.getElementById("location").innerHTML = locationDisplay;
+  }
+  
+  async function displayCurrentLocationWeather() {
+    const ipData = await fetchJsonData(ipUrl)
+    displayCurrentLocation(ipData)
+    
+    const {city, region, country} = ipData
+    const locationQuery = `${city}, ${region}, ${country}`;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationQuery}&appid=${OPENWEATHERMAP_API}`
+    const currentWeatherData = await fetchJsonData(weatherUrl)
+    displayWeather(currentWeatherData)
+  }
+
+  displayCurrentLocationWeather()
+  $('#form').trigger("reset");
+  $('input').val("")
+})
+
+$("#citySubmit").submit( e => {
+  e.preventDefault();
+  async function main(){
+    try {
+        e.preventDefault();
+        let inputVal = $('input').val();
+
+        const limit = 5
+        const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${inputVal}&limit=${limit}&appid=${OPENWEATHERMAP_API}`
+        const geoData = await fetchJsonData(geoUrl)
+        const {state, lat, lon, country} = geoData[0]
+        const geoName = geoData[0]['name']
+        const weatherLatLonUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHERMAP_API}`
+        const weatherData = await fetchJsonData(weatherLatLonUrl)
+
+        displayWeather(weatherData)
+
+        const flag = getFlagEmoji(country)
+        let regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+        const countryName = regionNames.of(country);
+
+        $("#location").html(`${flag}<br>${geoName}${state ? ', ' + state : ''}<br>${countryName} (${country})`);
+        $(".msg").text("");
+        $('#form').trigger("reset");
+        $('input').val("")
+    } catch (e) {
+        console.log(`error: ${e}`)
+        $(".msg").text("City not found")
+    }
+  }
+
+  main()
+});
